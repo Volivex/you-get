a4dcd9311514040baf2ba1e96a7a8dcedf4892ad
Volivex <Somoliver1@gmail.com>
2025-04-06 20:04:32 +0300

    added class for work with users Create function register and auth

--- users/test.py
+++ users/test.py
@@ -1,5 +1,12 @@
-def start():
-    return 'Регистарция пользователя'
 
-def second_step():
-    return 'Авторизация'
+class User:
+    def __init__(self, login, password):
+        self.login = login
+        self.password = password
+    def register(self):
+        return 'Регистарция пользователя'
+    def auth(self):
+        return f'Авторизация пользователя {self.login}'
+
+user1 = User(input("Введите логин: "), input("Введите пароль: "))
+print(user1.auth())
